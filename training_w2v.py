911042e4fbf76e384d1e0b40c4608e854c2f5271
Paulo <paulomarciano@gmail.com>
2018-05-27 19:49:12 -0300

    bugfixes...

--- training_w2v.py
+++ training_w2v.py
@@ -5,39 +5,62 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import os
+import glob
+import json
+import datetime
 from tqdm import tqdm
 from keras.preprocessing.sequence import skipgrams, make_sampling_table
 
 embedding_dim = 100
 batch_size = 10000
-epochs = 20
+epochs = 5
 
-fsp = pd.read_csv('/home/paulo/projetos/Seq2Seq/data/folha_de_sao_paulo/articles.csv')
+print("Loading Files... {}".format(datetime.datetime.time(datetime.datetime.now())))
+fsp = pd.read_csv('./data/folha_de_sao_paulo/articles.csv')
 docs = list(fsp.text.dropna())
+for folder in tqdm(os.listdir('./data/Wiki Text')):
+    gl = glob.glob(os.path.join('./data/Wiki Text', folder) + '/*')
+    for filename in gl:
+        with open(filename, 'r') as f:
+            for line in f:
+                docs.append(json.loads(line)['text'])
 
+print("Tokenizing docs... {}".format(datetime.datetime.time(datetime.datetime.now())))
 tokenizer = nlp.Tokenizer(oov_token='<UNK>')
 tokenizer.fit(docs)
+
 docs = tokenizer.transform(docs)
-vocab_size = len(tokenizer.word_index)+1
+vocab_size = len(tokenizer.word_index)
+
+print("Creating the model... {}".format(datetime.datetime.time(datetime.datetime.now())))
 w2v = nlp.Word2Vec(vocab_size,embedding_dim,device=torch.device('cuda:0'))
 sampling_table = make_sampling_table(vocab_size)
 
 couples = []
 targets = []
-for doc in docs[:10000]:
-    new_couples, new_targets = skipgrams(doc,
-                                         vocab_size,
-                                         sampling_table=sampling_table)
-    couples.extend(new_couples)
-    targets.extend(new_targets)
 
-words, contexts = zip(*couples)
+print("Training... {}".format(datetime.datetime.time(datetime.datetime.now())))
+for t in range(epochs):
+    print("epoch #: {}".format(t))
+    for i,doc in enumerate(docs):
+        new_couples, new_targets = skipgrams(doc,
+                                             vocab_size,
+                                             sampling_table=sampling_table)
+        couples.extend(new_couples)
+        targets.extend(new_targets)
 
-history = w2v.fit(words,
-                  contexts,
-                  targets,
-                  batch_size,
-                  epochs=epochs,
-                  optimizer=optim.Adam(w2v.parameters(),lr=1e-3))
+        if (i > 0 and i % 20000 == 0) or (i >= len(docs)-1):
+            words, contexts = zip(*couples)
+
+            history = w2v.fit(words,
+                              contexts,
+                              targets,
+                              batch_size,
+                              epochs=1,
+                              optimizer=optim.Adam(w2v.parameters(),lr=1e-3))
+
+            couples = []
+            targets = []
 
 w2v.save_embedding('./teste.pt')
